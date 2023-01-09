fb50ce01621c33248aee22f8b87a0740b958e3c8
Mohammad Ali <03340217mali@gmail.com>
2023-01-09 20:33:17 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  return (
+    <main></main>
+  );
+}
+
+export default App;
